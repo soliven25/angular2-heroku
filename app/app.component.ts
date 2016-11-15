d1d36dbfa964065601dbb280128a1c33867fba76
Sungkeun Choi <soliven25@gmail.com>
2016-11-15 19:51:09 +0900

    add ! for test after change app name in heroku

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: '<h1>This is First Angular 2 App with heroku</h1>'
+  template: '<h1>This is First Angular 2 App with heroku!</h1>'
 })
 export class AppComponent { }
